83547b561c020b5d36871c4d6057922c46b19d61
larramorelaura <may.lauraa@gmail.com>
2022-11-23 19:15:14 -0500

    update

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,4 +1,5 @@
 
+// creating an object I can change with the functions
 var calculator = {
     answer: "0",
     firstNumber : "0",
@@ -6,13 +7,19 @@ var calculator = {
     secondNumber : "0"
 };
 
+// variables used globally in functions
 var answer= 0;
-
-
-
 var display= document.querySelector('#display');
 var decimal= document.getElementById('decimal');
 
+// when a input button is pressed, this will
+// clear the 0 placeholder from the display,
+// disable the decimal button
+// if that is the one pushed to keep it from allowing
+// more than one decimal,
+// and then reset the object answer to 0 
+// so that we can clear the 0 for other operands
+// and in the end will push the input pressed into the display
 function press(num){
     if(display.innerText=="0" || calculator.answer>0){
         display.innerText ="";
@@ -24,6 +31,11 @@ function press(num){
     display.innerText += num;
 } 
 
+// this will set the operator to the input from the operator button
+// then assign the firstNumber a value within the object
+// log for check
+// then reset the display to 0
+// then enable the decimal so it can be used in a second operand
 function setOP(op){
     calculator.operator = op;
     calculator.firstNumber= display.innerText;
@@ -32,6 +44,13 @@ function setOP(op){
     decimal.disabled = false;
 }
 
+// this assigns secondNumber a value based on what is in the display
+// log check
+// parseFloat to make the strings numbers to be able to perform the math
+// choose which math to operate based on the value of the object operator
+// change the display to the answer
+// change the answer value in the object to the answer 
+// log checks
 function calculate(){
     calculator.secondNumber= display.innerText;
     console.log(calculator);
@@ -42,9 +61,9 @@ function calculate(){
     } else if (calculator.operator == '-'){
         answer= (newFirst.toFixed(8) *100000000 - newSecond.toFixed(8)* 100000000) / 100000000;
     } else if (calculator.operator == '*'){
-        answer = (newFirst.toFixed(8) *100000000 * newSecond.toFixed(8)* 100000000) / 100000000;
+        answer = ((newFirst.toFixed(8) *100000000)/ 100000000 * (newSecond.toFixed(8)* 100000000)/ 100000000) ;
     } else if (calculator.operator == '/'){
-        answer= (newFirst.toFixed(8) *100000000 / newSecond.toFixed(8)* 100000000) / 100000000;
+        answer= ((newFirst.toFixed(8) *100000000) / (newSecond.toFixed(8)* 100000000))/ 100000000;
     }
     display.innerText=answer;
     calculator.answer=answer;
@@ -52,12 +71,14 @@ function calculate(){
     console.log(answer);
 }
 
+// resets the object values and the display and enables the decimal and log checks the result
 function clr(){
     document.querySelector('#display').innerText ="0";
     calculator.answer="0";
     calculator.firstNumber="0";
     calculator.operator= null;
     calculator.secondNumber= "0";
+    decimal.disabled= false;
     console.log(calculator);
 }
 
---
+++ algos/w2d5/game.js
@@ -0,0 +1,77 @@
+var theDojo = [ [1, 0, 1, 1, 1, 0, 4, 0, 8, 0],
+                [3, 1, 0, 7, 0, 0, 6, 0, 8, 8],
+                [5, 0, 7, 0, 3, 6, 6, 6, 0, 0],
+                [2, 3, 0, 9, 0, 0, 6, 0, 8, 0],
+                [6, 0, 3, 3, 0, 2, 0, 3, 0, 4],
+                [0, 0, 3, 3, 0, 0, 2, 2, 3, 0],
+                [0, 0, 0, 0, 5, 0, 1, 2, 0, 6],
+                [2, 2, 2, 2, 0, 7, 1, 1, 1, 0],
+                [5, 2, 0, 2, 0, 0, 0, 1, 1, 2],
+                [9, 2, 2, 2, 0, 7, 0, 1, 1, 0] ];
+var dojoDiv = document.querySelector("#the-dojo");
+var mineCounter=0;
+
+// Creates the rows of buttons for this game
+function render(theDojo) {
+  var result = "";
+
+  for(var i=0; i<theDojo.length; i++) {
+    for(var j=0; j<theDojo[i].length; j++) {
+      if(mineCounter < 10 && !(theDojo[i].includes(1))){
+        theDojo[i][j]=Math.round(Math.random());
+        if(theDojo[i][j]==1){
+          mineCounter++;
+        }
+      }
+        else{theDojo[i][j]=0}
+      result += `<button class="tatami" onclick="howMany(${i}, ${j}, this); restart(${i}, ${j}, this);"></button>`;
+    }
+  }
+  console.log(theDojo);
+  return result;
+}
+    
+// TODO - Make this function tell us how many ninjas are hiding 
+//        under the adjacent (all sides and corners) squares.
+//        Use i and j as the indexes to check theDojo.
+function howMany(i, j, element) {
+    var count=0;
+    console.log({i, j});
+    for(let row= i-1; row<=i+1 && row<theDojo.length; row++){
+      if(row>=0){
+          for(let col=j-1; col<=j+1 && col<theDojo[0].length; col++){
+              if (!(row === i && col === j) && col>=0){
+              count+=theDojo[row][col];
+              }
+          }
+      }
+    }
+    console.log(count);
+    element.innerText= count;
+}
+
+
+
+  alert("Hello, welcome to Dojo Sweeper, avoid the hidden ninjas! Good luck! ");
+  function restart(i, j){
+    if (theDojo[i][j]==1){
+      dojoDiv.innerHTML = `<button onclick="location.reload()">restart</button>`;
+    }
+  }
+    
+// BONUS CHALLENGES
+// 1. draw the number onto the button instead of alerting it
+// 2. at the start randomly place 10 ninjas into theDojo with at most 1 on each spot
+// 3. if you click on a ninja you must restart the game 
+//    dojoDiv.innerHTML = `<button onclick="location.reload()">restart</button>`;
+    
+// start the game
+// message to greet a user of the game
+var style="color:cyan;font-size:1.5rem;font-weight:bold;";
+console.log("%c" + "IF YOU ARE A DOJO STUDENT...", style);
+console.log("%c" + "GOOD LUCK THIS IS A CHALLENGE!", style);
+// shows the dojo for debugging purposes
+console.table(theDojo);
+// adds the rows of buttons into <div id="the-dojo"></div> 
+dojoDiv.innerHTML = render(theDojo);    
+
